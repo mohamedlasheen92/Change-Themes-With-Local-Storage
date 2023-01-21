c53eb5514751d1b312154360cf28dd4be024ae53
mohamedlasheen92 <mohamedlasheen637@gmail.com>
2023-01-21 13:54:38 +0200

    UPD modify responsive resize

--- js/app.js
+++ js/app.js
@@ -1,18 +1,19 @@
 $(document).ready(function () {
-  console.log('Welcome Here');
+  console.log("Welcome Here");
 
-  $(window).resize(function () { 
+  $("body").css("padding-left", $(".sidebar").innerWidth() + 50);
+  $(window).resize(function () {
     // console.log($(window).innerWidth() + 200);
     if ($(window).innerWidth() > 767) {
-      $('body').css('padding-left', $('.sidebar').innerWidth() + 50);
+      $("body").css("padding-left", $(".sidebar").innerWidth() + 50);
     }
     // else {
     //   $('body').css('padding-left', 0);
     // }
   });
 
-  $('.colors li').click(function () {
-    $('body').attr('data-default-color', $(this).data('color'));
-  })
-  console.log($('.colors').innerWidth());
+  $(".colors li").click(function () {
+    $("body").attr("data-default-color", $(this).data("color"));
+  });
+  console.log($(".colors").innerWidth());
 });
